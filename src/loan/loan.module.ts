afee03d02123a9f02dc812c63bec6438797ef4a8
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-02-06 08:29:53 +0530

    feat: remove old modules

--- src/loan/loan.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LoanService } from './loan.service';
-import { LoanResolver } from './loan.resolver';
-
-@Module({
-  providers: [LoanResolver, LoanService],
-})
-export class LoanModule {}
--- src/underwriting/underwriting.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UnderwritingService } from './underwriting.service';
-
-@Module({
-  providers: [UnderwritingService]
-})
-export class UnderwritingModule {}
