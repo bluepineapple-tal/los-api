a0fe76911e0694ccfca2a3bd5f14d35415717b3b
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-02-08 19:58:47 +0530

    fix: load env file correctly

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -1,8 +1,15 @@
+import * as dotenv from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
+dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+
 export const appDataSource = new DataSource({
-  type: 'sqlite',
-  database: 'db.sqlite',
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
   entities: [__dirname + '/src/**/*.entity{.js,.ts}'],
   migrations: [__dirname + '/migrations/*{.js,.ts}'],
 } as DataSourceOptions);
