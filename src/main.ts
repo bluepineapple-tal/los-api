10ec4665da68ffeca5efdb3c246548ac3b3301e4
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-02-20 19:35:53 +0530

    build: bind listen to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     }),
   });
 
-  await app.listen(PORT);
+  await app.listen(PORT, '0.0.0.0');
+  console.log(`🚀 Server is running on port ${PORT}`);
 }
 bootstrap();
