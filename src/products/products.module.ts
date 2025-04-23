4c8342eb4c9e6ad316cf4c4f47c3f8034a44c51e
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-04-23 13:53:19 +0530

    feat: add product resolvers to graphql schema

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProductCategoriesController } from './product-categories/product-categories.controller';
+import { ProductCategoriesResolver } from './product-categories/product-categories.resolver';
 import { ProductCategoriesService } from './product-categories/product-categories.service';
 import { ProductCategory } from './product-categories/product-category.entity';
 import { ProductMakeController } from './product-make/product-make.controller';
 import { ProductMake } from './product-make/product-make.entity';
+import { ProductMakeResolver } from './product-make/product-make.resolver';
 import { ProductMakeService } from './product-make/product-make.service';
 import { ProductModelController } from './product-model/product-model.controller';
 import { ProductModel } from './product-model/product-model.entity';
+import { ProductModelResolver } from './product-model/product-model.resolver';
 import { ProductModelService } from './product-model/product-model.service';
 
 @Module({
@@ -17,8 +20,11 @@ import { ProductModelService } from './product-model/product-model.service';
   ],
   providers: [
     ProductMakeService,
+    ProductMakeResolver,
     ProductModelService,
+    ProductModelResolver,
     ProductCategoriesService,
+    ProductCategoriesResolver,
   ],
   controllers: [
     ProductMakeController,
